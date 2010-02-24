814f224397a20c4f7800022db63a0ad1746cf545
Fred Moyer <phred@apache.org>
2010-02-24 21:45:05 +0000

    start 1.32-dev cycle
    git-svn-id: http://svn.apache.org/repos/asf/perl/Apache-Test/trunk@915988 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache/Test.pm
+++ lib/Apache/Test.pm
@@ -36,7 +36,7 @@ BEGIN {
 
 use vars qw(@ISA @EXPORT %EXPORT_TAGS $VERSION %SubTests @SkipReasons);
 
-$VERSION = '1.31';
+$VERSION = '1.32';
 
 my @need = qw(need_lwp need_http11 need_cgi need_access need_auth
               need_module need_apache need_min_apache_version
