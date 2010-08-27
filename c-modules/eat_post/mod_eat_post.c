apache/httpd-tests

commit 4477ae5f2947c6c024a1aa805a43f2241687f562

- fix mismatched format string warnings throughout
git-svn-id: https://svn.apache.org/repos/asf/httpd/test/trunk/perl-framework@290160 13f79535-47bb-0310-9956-ffa450edef68

c-modules/echo_post/mod_echo_post.c
@@ -42,22 +42,23 @@ static int echo_post_handler(request_rec *r)
 #endif
     
     if (r->args) {
-        ap_rprintf(r, "%ld:", r->remaining);
+        ap_rprintf(r, "%" APR_OFF_T_FMT ":", r->remaining);
     }
 
-    fprintf(stderr, "[mod_echo_post] going to echo %ld bytes\n",
+    fprintf(stderr, "[mod_echo_post] going to echo %" APR_OFF_T_FMT " bytes\n",
             r->remaining);
 
     while ((nrd = ap_get_client_block(r, buff, sizeof(buff))) > 0) {
         fprintf(stderr,
-                "[mod_echo_post] read %ld bytes (wanted %d, remaining=%ld)\n",
+                "[mod_echo_post] read %ld bytes (wanted %" APR_SIZE_T_FMT 
+                ", remaining=%" APR_OFF_T_FMT ")\n",
                 nrd, sizeof(buff), r->remaining);
         ap_rwrite(buff, nrd, r);
         total += nrd;
     }
 
     fprintf(stderr,
-            "[mod_echo_post] done reading %ld bytes, %ld bytes remain\n",
+            "[mod_echo_post] done reading %ld bytes, %" APR_OFF_T_FMT " bytes remain\n",
             total, r->remaining);
     
     return OK;

c-modules/echo_post_chunk/mod_echo_post_chunk.c
@@ -39,15 +39,17 @@ static int echo_post_chunk_handler(request_rec *r)
     }
 
     if (r->args) {
-        ap_rprintf(r, "%ld:", r->remaining);
+        ap_rprintf(r, "%" APR_OFF_T_FMT ":", r->remaining);
     }
 
-    fprintf(stderr, "[mod_echo_post_chunk] going to echo %ld bytes\n",
+    fprintf(stderr, "[mod_echo_post_chunk] going to echo "
+            "%" APR_OFF_T_FMT " bytes\n",
             r->remaining);
 
     while ((nrd = ap_get_client_block(r, buff, sizeof(buff))) > 0) {
         fprintf(stderr,
-                "[mod_echo_post_chunk] read %ld bytes (wanted %d, remaining=%ld)\n",
+                "[mod_echo_post_chunk] read %ld bytes "
+                "(wanted %" APR_SIZE_T_FMT ", remaining=%" APR_OFF_T_FMT ")\n",
                 nrd, sizeof(buff), r->remaining);
         total += nrd;
     }
@@ -74,7 +76,8 @@ static int echo_post_chunk_handler(request_rec *r)
     ap_rputs(trailer_header, r);
 
     fprintf(stderr,
-            "[mod_echo_post_chunk] done reading %ld bytes, %ld bytes remain\n",
+            "[mod_echo_post_chunk] done reading %ld bytes, "
+            "%" APR_OFF_T_FMT " bytes remain\n",
             total, r->remaining);
     
     return OK;

c-modules/test_pass_brigade/mod_test_pass_brigade.c
@@ -39,7 +39,8 @@ static int test_pass_brigade_handler(request_rec *r)
     httpd_test_split_qs_numbers(r, &buff_size, &remaining, NULL);
 
     fprintf(stderr, "[mod_test_pass_brigade] "
-            "going to echo %lu bytes with buffer size=%lu\n",
+            "going to echo %" APR_SIZE_T_FMT " bytes with "
+            "buffer size=%" APR_SIZE_T_FMT "\n",
             remaining, buff_size);
 
     buff = malloc(buff_size);
@@ -75,7 +76,8 @@ static int test_pass_brigade_handler(request_rec *r)
     
     apr_brigade_destroy(bb);
     fprintf(stderr,
-            "[mod_test_pass_brigade] done writing %ld of %ld bytes\n",
+            "[mod_test_pass_brigade] done writing %" APR_SIZE_T_FMT 
+            " of %" APR_SIZE_T_FMT " bytes\n",
             total, remaining);
 
     free(buff);    

c-modules/test_rwrite/mod_test_rwrite.c
@@ -36,7 +36,7 @@ static int test_rwrite_handler(request_rec *r)
 
     httpd_test_split_qs_numbers(r, &buff_size, &remaining, NULL);
 
-    fprintf(stderr, "[mod_test_rwrite] going to echo %ld bytes\n",
+    fprintf(stderr, "[mod_test_rwrite] going to echo %" APR_SIZE_T_FMT " bytes\n",
             remaining);
 
     buff = malloc(buff_size);
@@ -53,7 +53,8 @@ static int test_rwrite_handler(request_rec *r)
     }
     
     fprintf(stderr,
-            "[mod_test_rwrite] done writing %ld of %ld bytes\n",
+            "[mod_test_rwrite] done writing %" APR_SIZE_T_FMT 
+            " of %" APR_SIZE_T_FMT " bytes\n",
             total, remaining);
 
     free(buff);    

tksst-obsolutes/httpd-ci

commit 45f832404043e38826b67014ba8b9794e4b05f9a

- Remove ifdefs from within ap_log_* calls.   Fixes compilation problem with xlc on AIX   against httpd 2.3.8+. - Reintroduce Apache 1.3 compatibility in mod_echo_post   (broken in r290160 and r947590).
git-svn-id: http://svn.apache.org/repos/asf/httpd/test/framework/trunk@990065 13f79535-47bb-0310-9956-ffa450edef68

c-modules/eat_post/mod_eat_post.c
@@ -31,12 +31,13 @@ static int eat_post_handler(request_rec *r)
     }
 
     if ((rc = ap_setup_client_block(r, REQUEST_CHUNKED_ERROR)) != OK) {
-        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO,
-#ifdef APACHE2
-                     0,
-#endif /* APACHE2 */
-                     r->server,
+#ifdef APACHE1
+        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, r->server,
                      "[mod_eat_post] ap_setup_client_block failed: %d", rc);
+#else
+        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, 0, r->server,
+                     "[mod_eat_post] ap_setup_client_block failed: %d", rc);
+#endif /* APACHE1 */
         return rc;
     }
 

c-modules/echo_post/mod_echo_post.c
@@ -24,12 +24,13 @@ static int echo_post_handler(request_rec *r)
     }
 
     if ((rc = ap_setup_client_block(r, REQUEST_CHUNKED_ERROR)) != OK) {
-        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO,
-#ifdef APACHE2
-                     0,
-#endif /* APACHE2 */
-                     r->server,
+#ifdef APACHE1
+        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, r->server,
                      "[mod_echo_post] ap_setup_client_block failed: %d", rc);
+#else
+        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, 0, r->server,
+                     "[mod_echo_post] ap_setup_client_block failed: %d", rc);
+#endif /* APACHE1 */
         return 0;
     }
 
@@ -42,31 +43,58 @@ static int echo_post_handler(request_rec *r)
 #endif
     
     if (r->args) {
+#ifdef APACHE1
+        ap_rprintf(r, "%ld:", r->remaining);
+#else
         ap_rprintf(r, "%" APR_OFF_T_FMT ":", r->remaining);
+#endif /* APACHE1 */
     }
 
+#ifdef APACHE1
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, r,
+                  "[mod_echo_post] going to echo %ld bytes",
+                  r->remaining);
+#else
     ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
                   "[mod_echo_post] going to echo %" APR_OFF_T_FMT " bytes",
                   r->remaining);
+#endif /* APACHE1 */
 
     while ((nrd = ap_get_client_block(r, buff, sizeof(buff))) > 0) {
+#ifdef APACHE1
+        ap_log_rerror(APLOG_MARK, APLOG_DEBUG, r,
+                      "[mod_echo_post] read %ld bytes (wanted %d, remaining=%ld)",
+                      nrd, sizeof(buff), r->remaining);
+#else
         ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
                       "[mod_echo_post] read %ld bytes (wanted %" APR_SIZE_T_FMT 
                       ", remaining=%" APR_OFF_T_FMT ")",
                       nrd, sizeof(buff), r->remaining);
+#endif /* APACHE1 */
         ap_rwrite(buff, nrd, r);
         total += nrd;
     }
 
     if (nrd < 0) {
         ap_rputs("!!!ERROR!!!", r);
+#ifdef APACHE1
+        ap_log_rerror(APLOG_MARK, APLOG_DEBUG, r,
+                      "[mod_echo_post] ap_get_client_block got error");
+#else
         ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
                       "[mod_echo_post] ap_get_client_block got error");
+#endif /* APACHE1 */
     }
 
+#ifdef APACHE1
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, r,
+            "[mod_echo_post] done reading %ld bytes, %ld bytes remain",
+            total, r->remaining);
+#else
     ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
             "[mod_echo_post] done reading %ld bytes, %" APR_OFF_T_FMT " bytes remain",
             total, r->remaining);
+#endif /* APACHE1 */
     
     return OK;
 }

c-modules/echo_post_chunk/mod_echo_post_chunk.c
@@ -25,12 +25,13 @@ static int echo_post_chunk_handler(request_rec *r)
     }
 
     if ((rc = ap_setup_client_block(r, REQUEST_CHUNKED_DECHUNK)) != OK) {
-        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO,
-#ifdef APACHE2
-                     0,
-#endif /* APACHE2 */
-                     r->server,
+#ifdef APACHE1
+        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, r->server,
                      "[mod_echo_post_chunk] ap_setup_client_block failed: %d", rc);
+#else
+        ap_log_error(APLOG_MARK, APLOG_ERR|APLOG_NOERRNO, 0, r->server,
+                     "[mod_echo_post_chunk] ap_setup_client_block failed: %d", rc);
+#endif /* APACHE1 */
         return 0;
     }
 